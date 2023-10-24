295c67d9fd5b0043952cdaf715e6b6a58a0eecf0
ftoblare <148501411+ftoblare@users.noreply.github.com>
2023-10-24 17:55:54 +0100

    Creado index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$nombre = $_REQUEST['nombre'];
+$fNacimiento = $_REQUEST['nacimiento'];
+$sexo = $_REQUEST['sexo'];
+
+echo "Nombre: ".$nombre."<br/>";
+echo "Fecha de nacimiento: ".$fNacimiento."<br/>";
+echo "Sexo: ".$sexo."<br/>";
+?>
+<hr/>
+
+<?php
+echo "Ficha del Sr./Sra " . $nombre . " (sexo:" . $sexo . ") nacido el " . $fNacimiento . ".<br/>" 
+?>
+
+<hr/>
+<?php
+foreach($_REQUEST as $v => $valor){
+    echo "Elemento del formulario con name=".$v.": ".$valor."<br/>" ;
+}
+?>
